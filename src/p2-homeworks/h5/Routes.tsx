18c05d4921b9ec789acf45b12218b3eb74c1ee80
Nikita Stasiulevich <nikitastas1694@gmail.com>
2022-01-21 21:51:33 +0300

    add react-router-dom 5.3.1

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Redirect, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import PreJunior from "./pages/PreJunior";
 import Error404 from "./pages/Error404";
 
@@ -11,17 +11,20 @@ export const PATH = {
 function Routes() {
     return (
         <div>
-            Switch выбирает первый подходящий роут
+            <BrowserRouter>
+                <Switch></Switch>
+            </BrowserRouter>
+            {/*Switch выбирает первый подходящий роут*/}
             <Switch>
 
-            в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR
-            exact нужен чтоб указать полное совподение (что после '/' ничего не будет)
+            {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
+            {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
             <Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
 
             <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
                 // add routes
 
-            у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу
+            {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
             <Route render={() => <Error404/>}/>
 
             </Switch>
