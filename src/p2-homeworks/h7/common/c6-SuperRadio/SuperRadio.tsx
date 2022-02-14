bbf0715e974c7aebf16e274994414e871fab1bb0
Nikita Stasiulevich <nikitastas1694@gmail.com>
2022-02-14 21:51:01 +0300

    сщcompleted HW7

--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -16,14 +16,18 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // onChange, onChangeOption
+       onChangeOption && onChangeOption(e);
     }
 
 
-    const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
+    const mappedOptions: any[] = options ? options.map((o, i) => (
         <label key={name + '-' + i}>
             <input
                 type={'radio'}
+                name={name}
+                checked={value === o}
+                value={o}
+                onChange={() => onChangeCallback(o)}
                 // name, checked, value, onChange
             />
             {o}
